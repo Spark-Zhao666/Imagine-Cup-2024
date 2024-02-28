dc8c8c0f2516ec01642ead9e5daee90baa837a40
Spark Zhao <diosparkbrando@gmail.com>
2024-02-28 15:05:20 +0800

    Give out AI
    Miller

---
+++ Sleep AI.py
@@ -0,0 +1,21 @@
+import torch.nn as nn
+import torch.functional as F
+
+class Inception(nn.modules):
+    def __init__(self):
+        super().__init__()
+
+    
+class Sleep_CNN(nn.modules):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1,1,7)
+        self.bn1 = nn.BatchNorm2d(256)
+        self.mp1 = nn.MaxPool2d(3,2)
+        
+        self.conv2 = nn.Conv2d(1,1,3)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.mp2 = nn.MaxPool2d(3,2)
+
+        Larry
+
